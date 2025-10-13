b32272141d8732170733a34edbd674a0ec7029d6
Gabriela Moroyoqui <thegabster309@gmail.com>
2025-10-13 01:32:23 -0700

    feat: working battle (FF1 style run), UI over background, stable playthrough

--- Assets/Scenes/UnitStats.cs
+++ Assets/Scenes/UnitStats.cs
@@ -1,28 +1,15 @@
 using UnityEngine;
 
-namespace FF.Battle
+[System.Serializable]
+public class UnitStats
 {
-    [System.Serializable]
-    public class UnitStats
-    {
-        public string Name;
-        public int MaxHP, HP, Attack, Defense;
-        public bool IsDead => HP <= 0;
+    public string Name;
+    public int MaxHP, HP, Attack, Defense;
+    public bool IsDead => HP <= 0;
 
-        public static UnitStats Hero(string name, int hp, int atk, int def)
-            => new UnitStats { Name = name, MaxHP = hp, HP = hp, Attack = atk, Defense = def };
+    public static UnitStats Hero(string name, int hp, int atk, int def)
+        => new UnitStats { Name = name, MaxHP = hp, HP = hp, Attack = atk, Defense = def };
 
-        public static UnitStats Enemy(string name, int hp, int atk, int def)
-            => new UnitStats { Name = name, MaxHP = hp, HP = hp, Attack = atk, Defense = def };
-    }
-
-    public enum CommandType { Fight, Run }
-
-    public struct QueuedAction
-    {
-        public bool actorIsPlayer;
-        public int actorIndex;
-        public CommandType type;
-        public int targetIndex;
-    }
+    public static UnitStats Enemy(string name, int hp, int atk, int def)
+        => new UnitStats { Name = name, MaxHP = hp, HP = hp, Attack = atk, Defense = def };
 }
